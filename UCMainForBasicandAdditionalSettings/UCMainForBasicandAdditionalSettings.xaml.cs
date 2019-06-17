df29a95344beadb042e88230e41218745ad0b4e7
Ivan K (from work) <vania.ru.ru@mail.ru>
2019-06-17 11:06:02 +0300

    #внесены изменения

--- UCMainForBasicandAdditionalSettings/UCMainForBasicandAdditionalSettings.xaml.cs
+++ UCMainForBasicandAdditionalSettings/UCMainForBasicandAdditionalSettings.xaml.cs
@@ -1,19 +1,8 @@
-﻿using LocalizationBasicAndAdditionalSettings.Localization;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using UCAdditionalSettings;
 using UCBasicSettings;
 
@@ -29,6 +18,11 @@ namespace UCMainSettingsUMTS
             InitializeComponent();
             ComboBox_SwitchLocalization.DataContext = new VMUCMainForBasicandAdditionalSettings();
 
+            SetUserControlsInfo();           
+        }
+
+        private void SetUserControlsInfo()
+        {
             #region Set UserControlSettingsRAN
             CurrentOptionsUMTSInfo.CurrentBasicSettingsInfo = new BasicSettingsInfo()
             {
